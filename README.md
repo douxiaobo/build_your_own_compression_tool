c071bba01e400a87eede41aa21820f6b9bcb2ed3
douxiaobo <dxb_1020@icloud.com>
2024-06-15 15:53:06 +0800

    success step1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Build Your Own Compression Tool
+
+## Rust
+
+### June 15
+
+Success with step1.
---
+++ rust/step1.rs
@@ -0,0 +1,19 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::collections::HashMap;
+fn main(){
+    let path=Path::new("../135-0.txt");
+    let file=File::open(path).expect("Failed to open file");
+    let reader=BufReader::new(file);
+    let mut character:HashMap<char,u32> = HashMap::new();
+    for line in reader.lines(){
+        if let Ok(line_contents)=line{
+            for ch in line_contents.chars(){
+                *character.entry(ch).or_insert(0) += 1;
+            }
+        }    
+    }
+    println!("X: {},t: {}",character[&'X'],character[&'t']);
+}
